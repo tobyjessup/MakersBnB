19229cb44bd2075358c7e73e9c761beb70a361b5
Phil Smithies <philsmithies@gmail.com>
2021-04-27 15:50:33 +0200

    listing.create added

--- lib/listing.rb
+++ lib/listing.rb
@@ -1,7 +1,18 @@
 require_relative 'database_connection'
 
 class Listing
-  def self.create(name:, description:, price:)
 
+  attr_reader :name, :description, :price, :id
+
+  def initialize(id:, name:, description:, price:)
+    @id = id
+    @name = name
+    @description = description
+    @price = price
+  end
+
+  def self.create(name:, description:, price:)
+    result = DatabaseConnection.query("INSERT INTO listing (name, description, price) VALUES('#{name}', '#{description}', '#{price}') RETURNING id, name, description, price;")
+    Listing.new(id: result[0]['id'], name: result[0]['name'], description: result[0]['description'], price: result[0]['price'])
   end
 end
--- spec/unit/listing_spec.rb
+++ spec/unit/listing_spec.rb
@@ -3,13 +3,19 @@ require 'listing'
 describe Listing do
   context('#create') do 
     it('Allows the user to create a listing') do 
-      PG.connect(dbname: 'makersbnb_test')
+      # PG.connect(dbname: 'makersbnb_test')
       listing = Listing.create(name: 'name', description: 'description', price: 100)
+      persisted_data = persisted_data(table: 'listing', id: listing.id)
       expect(listing).to be_a Listing
-      expect(listing.length).to eq(1)
-      expect(listing.name).to eq('name')
-      expect(listing.description).to eq('description')
-      expect(listing.price).to eq(100)
+      expect(listing.id).to eq(persisted_data.first['id'])
     end
+    # context('#all') do 
+    #   it 'Allows the user to see all the listings' do 
+    #     expect(listing).to be_a Listing
+    #     expect(listing.length).to eq(1)
+    #     expect(listing.name).to eq('name')
+    #     expect(listing.description).to eq('description')
+    #     expect(listing.price).to eq(100)
+    #   end
   end
 end
