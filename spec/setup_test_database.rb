9632de93da7e1442549e74f29720de3e991b7c21
YohannTisserand <tisserand.yohann@gmail.com>
2021-04-28 14:38:46 +0100

    solved 12 errors in one word

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -2,5 +2,5 @@ require 'pg'
 
 def setup_test_database
   connection = PG.connect(dbname: 'makersbnb_test')
-  connection.exec("TRUNCATE users, listing")
+  connection.exec("TRUNCATE users, listing, booking")
 end
