e2108160f1863c0e020a799ce3f5523266c4feb0
Phil Smithies <philsmithies@gmail.com>
2021-04-29 13:11:58 +0200

    fix push

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -2,5 +2,5 @@ require 'pg'
 
 def setup_test_database
   connection = PG.connect(dbname: 'makersbnb_test')
-  connection.exec("TRUNCATE users, listing, booking")
+  connection.exec("TRUNCATE users, listing, booking, availabilty")
 end
